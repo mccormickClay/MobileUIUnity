dc45747489773efc5322a573661a4b2238e8e648
mccormickClay <mcc-clay@hotmail.com>
2018-11-05 23:34:09 -0600

    First Commit

---
+++ MobileUIRPG/Assets/Scripts/messageAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+
+public class messageAttack : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    MethodBase method;
+
+	// Use this for initialization
+	void Awake () {
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(Attack);
+	}
+	
+    public void Attack()
+    {
+        method = MethodBase.GetCurrentMethod();
+        Debug.Log(gameObject.name + " -> " + method.Name);
+    }
+}
---
+++ MobileUIRPG/Assets/Scripts/messageDefend.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+
+public class messageDefend : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    MethodBase method;
+
+    // Use this for initialization
+    void Awake () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(Defend);
+    }
+
+    public void Defend()
+    {
+        method = MethodBase.GetCurrentMethod();
+        Debug.Log(gameObject.name + " -> " + method.Name);
+    }
+}
