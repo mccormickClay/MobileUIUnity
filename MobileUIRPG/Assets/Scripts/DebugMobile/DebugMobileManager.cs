15c28c4e459cb433ea3e1145fc0a9cae4d4aa41a
Clay McCormick <mcc-clay@hotmail.com>
2018-11-11 15:03:59 -0600

    Canvas creation
    Debugger now makes its own canvas to avoid messing with any of the game code.

--- MobileUIRPG/Assets/Scripts/DebugMobile/DebugMobileManager.cs
+++ MobileUIRPG/Assets/Scripts/DebugMobile/DebugMobileManager.cs
@@ -31,20 +31,58 @@ public class DebugMobileManager
 
     void privSetDebuggerText()
     {
-        GameObject canvas = GameObject.Find("Canvas");
         GameObject obj = new GameObject();
-        obj.name = "Debug Log";
+        privSetText(ref obj);
+
+        GameObject canvas = new GameObject();
+        privSetCanvas(ref canvas);
+
+        obj.transform.parent = canvas.transform;
+
+        obj.GetComponent<RectTransform>().localPosition = new Vector3(-400f, 890f, 0f);
+
+        //debuggerText = Object.FindObjectOfType<Text>();
+        //debuggerText.text = "";
+    }
+
+    void privSetText(ref GameObject obj)
+    {
+        obj.name = "DebugLog";
         obj.layer = LayerMask.NameToLayer("UI");
         obj.AddComponent<CanvasRenderer>();
         debuggerText = obj.AddComponent<Text>();
         debuggerText.fontSize = 70;
+        debuggerText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
         debuggerText.horizontalOverflow = HorizontalWrapMode.Overflow;
         debuggerText.verticalOverflow = VerticalWrapMode.Overflow;
         debuggerText.resizeTextForBestFit = false;
+        debuggerText.supportRichText = true;
+        debuggerText.color = Color.red;
+        debuggerText.text = "DEBUGGER ACTIVE";
+    }
 
+    void privSetCanvas(ref GameObject canvas)
+    {
+        canvas.name = "DebugCanvas";
+        canvas.layer = LayerMask.NameToLayer("UI");
 
-        //debuggerText = Object.FindObjectOfType<Text>();
-        //debuggerText.text = "";
+        RectTransform rect = canvas.AddComponent<RectTransform>();
+        rect.position = new Vector3(137f, 244f, 0f);
+        rect.sizeDelta = new Vector2(1080f, 1920f);
+        rect.pivot = new Vector2(.5f, .5f);
+        rect.localScale = new Vector3(0.25f,0.25f,0.25f);
+
+        Canvas can = canvas.AddComponent<Canvas>();
+        can.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        CanvasScaler cans = canvas.AddComponent<CanvasScaler>();
+        cans.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        cans.referenceResolution = new Vector2(1080f, 1920f);
+        cans.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        cans.referencePixelsPerUnit = 100f;
+
+        GraphicRaycaster graphicRaycaster = canvas.AddComponent<GraphicRaycaster>();
+        graphicRaycaster.ignoreReversedGraphics = true;
     }
 
     public static void Log(string _info)
