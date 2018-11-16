540c71701d6a3d7e49f899dbc43505194e4fc843
mccormickClay <mcc-clay@hotmail.com>
2018-11-15 20:04:08 -0600

    Added Debug Prefab
    bad canvas prefab for debug instead of making it all by code. easier to test and visually see

--- MobileUIRPG/Assets/Scripts/DebugMobile/DebugMobileManager.cs
+++ MobileUIRPG/Assets/Scripts/DebugMobile/DebugMobileManager.cs
@@ -30,61 +30,10 @@ public class DebugMobileManager
 
     void privCreate()
     {
-        GameObject obj = new GameObject();
-        privSetText(ref obj);
-
-        GameObject canvas = new GameObject();
-        privSetCanvas(ref canvas);
-
-        obj.transform.SetParent(canvas.transform, false);
-
-        obj.GetComponent<RectTransform>().pivot = Vector2.up;
-
-        Vector2 pos = DisplayManager.GetPositionOnScreen(0.0f, 1.0f);
-
-        obj.GetComponent<RectTransform>().position = pos;
-
-    }
-
-    void privSetText(ref GameObject obj)
-    {
-        obj.name = "DebugLog";
-        obj.layer = LayerMask.NameToLayer("UI");
-        obj.AddComponent<CanvasRenderer>();
-        debuggerText = obj.AddComponent<Text>();
-        debuggerText.fontSize = 20;
-        debuggerText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-        debuggerText.horizontalOverflow = HorizontalWrapMode.Overflow;
-        debuggerText.verticalOverflow = VerticalWrapMode.Overflow;
-        debuggerText.resizeTextForBestFit = false;
-        debuggerText.supportRichText = true;
-        debuggerText.color = Color.red;
-
-        debuggerText.text = "DEBUGGER ACTIVE";
-    }
-
-    void privSetCanvas(ref GameObject canvas)
-    {
-        canvas.name = "DebugCanvas";
-        canvas.layer = LayerMask.NameToLayer("UI");
-
-        RectTransform rect = canvas.AddComponent<RectTransform>();
-        rect.position = new Vector3(137f, 244f, 0f);
-        rect.sizeDelta = DisplayManager.GetAspectRatio();
-        rect.pivot = new Vector2(.5f, .5f);
-        rect.localScale = new Vector3(0.25f,0.25f,0.25f);
-
-        Canvas can = canvas.AddComponent<Canvas>();
-        can.renderMode = RenderMode.ScreenSpaceOverlay;
-
-        CanvasScaler cans = canvas.AddComponent<CanvasScaler>();
-        cans.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-        cans.referenceResolution = DisplayManager.GetAspectRatio();
-        cans.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
-        cans.referencePixelsPerUnit = 100f;
-
-        GraphicRaycaster graphicRaycaster = canvas.AddComponent<GraphicRaycaster>();
-        graphicRaycaster.ignoreReversedGraphics = true;
+        //GameObject debugUI = Resources.Load("CalledPrefabs/DebugCanvas") as GameObject;
+        GameObject debugUI = GameObject.Instantiate(Resources.Load("CalledPrefabs/DebugCanvas") as GameObject);
+        debugUI.name = "DebugCanvas";
+        debuggerText = debugUI.transform.GetChild(0).GetComponent<Text>();
     }
 
     public static void Log(string _info)
